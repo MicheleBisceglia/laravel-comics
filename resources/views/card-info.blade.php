b3163be049d84948e5ebdc8280ce85eac8388e50
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-06-27 18:14:45 +0200

    card-info structure

--- resources/views/card-info.blade.php
+++ resources/views/card-info.blade.php
@@ -1,13 +1,21 @@
 @extends('layout.app')
 
 @section('main_content')
-<h1>info card</h1>
+<section class="card-info">
+  <div class="card-info-top">
+    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+  </div>
+  <div class="card-info-bottom">
+    <div>
+      <h2>Action Comics #1000: The Deluxe Edition</h2>
+      <button>ciaociao</button><button>ciaociao</button>
+      <p class="description">The celebration of 1,000 issues of Action Comics continues with a new, Deluxe Edition of the amazing comic that won raves when it hit comics shops in April! This hardcover includes all the stories from that issue, plus the tale by writer Paul Levitz and artist Neal Adams that appeared in the Action Comics: 80 Years Of Superman hardcover, as well as all the variant covers, design sketches by Jim Lee for Superman’s new look, scripts for the stories, the original art from the lost story featuring art by Curt Swan and more! Plus: a complete reprint of the stories that started it all—the Superman stories Action Comics #1 and 2 from 1938!</p>
+    </div>
+    <div>
+      <p id="advertisement">ADVERTISEMENT</p>
+      <img src="../images/adv.jpg" alt="">
+    </div>
+  </div>
+</section>
 @endsection
 
-<style>
-  h1 {
-    height: 400px;
-    text-align: center;
-    color: white
-  }
-</style>
